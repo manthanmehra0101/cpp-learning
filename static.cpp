cba8b1e6017b70f189069f71121fc2a32c67f877
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-07 13:18:13 +0000

    Example of static and local static

--- static.cpp
+++ static.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <string>
 int count=10; // Declaration of the external variable 'count' in stat.cpp
 // if we declare count as static in stat.cpp, it will not be accessible in other files, and we will get a linker error when trying to use it in Base_class.cpp. To fix
 // this, we should remove the static keyword from the declaration of count in stat.cpp and declare it as an external variable in Base_class.cpp using the extern keyword. This way, both files can access the same variable without any linker errors.
 void displayCount() {
     std::cout << "Count: " << count << std::endl;
 }   
+class sun{
+    public:
+        static sun& get(){
+            static sun i;
+            return i;
+        }
+        void hi() {
+            std::cout << "Hello!\n";
+        }
+};
 int main() {
+    sun::get().hi();
     displayCount(); // Call the function to display the value of 'count'
     return 0;
 }

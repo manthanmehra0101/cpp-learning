6338b44bba9221f2c8de86df41614cf8a870e09d
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-08 08:12:48 +0000

    Example of static linking of gkfw

--- hi_dependency.cpp
+++ hi_dependency.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 //#include <GLFW/glfw3.h>
+/*
+The compiler only needs the declaration.
+
+The linker later finds the actual implementation
+inside GLFW library.
+
+Without the library we get linker errors.
+
+Without the declaration we get compiler errors.
+*/
 extern "C" int glfwInit();
 int main(){
     int a = glfwInit();

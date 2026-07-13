aa152e63fca66c7c02dd47e7677928f778d9d09b
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-13 06:43:48 +0000

    Example of array and some functions

--- array.cpp
+++ array.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <array>
+template<typename s>
+void p(const s& data){
+    for(int i = 0; i < data.size(); i++){
+        std::cout << data[i] << " ";
+    }
+}
 int main(){
     //creating raw array like this initializes array in stack memory of ram , in which
     //array will be deleted automatically after return 0
@@ -27,12 +33,13 @@ int main(){
     //method of standard array , and can now use size() to know number of elements in array
     //above two methods need something like 'int size = sizeof(arr)/sizeof(int)' to know 
     //number of elements and can't use size()
-    std::array<int,10> boring = {0};
+    std::array<int,10> boring = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::cout << "Third_Array : " ;
-    for (int i = 0; i < boring.size(); i++){
+    /*for (int i = 0; i < boring.size(); i++){
         boring[i] = 8;
         std::cout  <<boring[i] << " ";
-    }
+    }*/
+    p(boring);
     std::cout << std::endl;
     return 0;
 }

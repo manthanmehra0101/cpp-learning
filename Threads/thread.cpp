ed8d773f7080ba6b26cd3e4d3ee9ccc93a6d5170
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-15 13:41:48 +0000

    Example of threads and some other concepts

---
+++ Threads/thread.cpp
@@ -0,0 +1,33 @@
+/* Topics includes concept of-
+std::thread
+lambda
+macros
+chrono literals
+*/
+#include <iostream>
+#include <functional>
+#include <string>
+#include <thread>
+#define PR_DEBUG 1
+#if defined(PR_DEBUG)
+#define print(a) std::cout << a << "\n"
+#endif
+int main(){
+    
+    bool executed = false;
+    std::string a = "Working";
+    auto run = [&](){
+        do{
+            using namespace std::literals::chrono_literals;
+            print(a);
+            std::this_thread::sleep_for(1s);
+        }while(!executed);
+    };
+    std::thread console_print(run);
+    std::cin.get();
+    executed = true;
+    console_print.join();
+    print("Done!");
+    std::cin.get();
+    return 0;
+}

12bae67f3e3299f2f81e4a15f05d13f083cb4c14
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-17 06:27:25 +0000

    Example of standard sorting

--- STL/Algorithms/std_sort.cpp
+++ STL/Algorithms/std_sort.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
+template<typename k>
+void print(k& x){
+    for (int val : x){
+        std::cout << val << " ";
+    }
+}
 int main(){
     std::vector<int> vec = {9,1,4,3,5,8,6,2,7};
     std::cout << "Sorting 1 :\n";
     std::sort(vec.begin(), vec.end());
-    for (int val : vec){
-        std::cout << val << " ";
-    }
+    print(vec);
     std::cout << "\nSorting 2 :\n"; 
     std::sort(vec.begin(), vec.end(), [](int a, int b){
         return a>b;
     });
-    for (int val : vec){
-        std::cout << val << " ";
-    }
+    print(vec);
     std::cout << "\nAlternative Sorting 2 :\n";
     std::sort(vec.begin(), vec.end(), std::greater<int>());
-    for (int val : vec){
-        std::cout << val << " ";
-    }
+    print(vec);
     std::cout << "\nSorting 3 :\n";
     std::sort(vec.begin(), vec.end(), [](int a, int b){
         if (a==5){ return false; }
         if (b==1){ return true; }
         return a<b;
     });
-    for (int val : vec){
-        std::cout << val << " ";
-    }
+    print(vec);
     std::cin.get();
     return 0;
 }

c4bf5fd089f85c046a0adacc27275a1154d76150
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-29 11:16:50 +0000

    Example of multi-threading and atomic

--- Threads/thread.cpp
+++ Threads/thread.cpp
@@ -3,28 +3,30 @@ std::thread
 lambda
 macros
 chrono literals
+std::atomic with store() and load()
 */
 #include <iostream>
 #include <string>
 #include <thread>
+#include <atomic>
 #define PR_DEBUG 1
 #if defined(PR_DEBUG)
 #define print(a) std::cout << a << "\n"
 #endif
 int main(){
-    bool executed = false;
+    std::atomic<bool> executed{false};
     std::string a = "Working";
     auto run = [&](){
         do{
             using namespace std::literals::chrono_literals;
             print(a);
             std::this_thread::sleep_for(1s);
-        }while(!executed);
+        }while(!executed.load());
     };
     std::thread console_print(run);
     print(std::this_thread::get_id());
     std::cin.get();
-    executed = true;
+    executed.store(true);
     console_print.join();
     print("Done!");
     return 0;

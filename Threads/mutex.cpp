30b519f841bacd929db33703ccaf2c8188af7c06
Manthan Mehra <manthanmehra0101@gmail.com>
2026-07-30 11:13:01 +0000

    Example of simple mutex with manual lock & unlock as well as lock_guard

--- Threads/mutex.cpp
+++ Threads/mutex.cpp
@@ -5,7 +5,15 @@
 std::mutex m;
 int counter = 0;
 void work(){
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 2; i++){
+        m.lock();
+        counter++;
+        std::cout << "Thread " << std::this_thread::get_id() << " Counter : " << counter << "\n";
+        m.unlock();
+    }
+}
+void work2(){
+    for(int i = 0; i < 2; i++){
         std::lock_guard<std::mutex> lock(m);
         counter++;
         std::cout << "Thread " << std::this_thread::get_id() << " Counter : " << counter << "\n";
@@ -14,8 +22,12 @@ void work(){
 int main(){
     std::thread t1(work);
     std::thread t2(work);
+    std::thread t3(work2);
+    std::thread t4(work2);
     t1.join();
     t2.join();
+    t3.join();
+    t4.join();
     std::cout << "Final Counter : " << counter;
     std::cin.get();
 }
